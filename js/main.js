d5762c9661bb7b2a6ded19184128c8c12149772e
LEEJUNGHEE <qorhvk955@gamil.com>
2024-08-23 13:30:36 +0900

    시가초과 기능 추가

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ const scoreDisplay = document.querySelector("#score");
 const timeDisplay = document.querySelector("#time");
 const messageDisplay = document.querySelector("#message");
 
+const GAME_TIME = 5;
+
 let score = 0;
 let words = ["banana", "key", "car", "javascript", "junghee"];
+let time = 0;
+let timeInterval;
+let isPlaying = false;
 
 wordInput.addEventListener("input", (e) => {
   const typedText = e.target.value;
@@ -16,11 +21,33 @@ wordInput.addEventListener("input", (e) => {
   }
 });
 
+function gameover() {
+  console.log("gameover");
+  clearInterval(timeInterval);
+  isPlaying = false;
+  timeInterval = null;
+  score = 0;
+}
+
+function countDown() {
+  console.log(time);
+  time = time - 1;
+  timeDisplay.innerText = time;
+  if (time === 0) gameover();
+}
+
 function setNewWord() {
+  time = GAME_TIME;
+
   wordInput.value = "";
   messageDisplay.innerText = "NowPlaying!!";
   const randomIndex = Math.floor(Math.random() * words.length);
   currentWord.innerText = words[randomIndex];
+
+  if (!isPlaying) {
+    timeInterval = setInterval(countDown, 1000);
+    isPlaying = true;
+  }
 }
 
 function addScore() {
